208087f639de78e36efe10ae22b658165e2c37b6
Ach Izalul Haq <izalul@mit.id>
2023-04-11 07:33:27 +0700

    Update. change credential location

--- data.php
+++
@@ -1,9 +0,0 @@
-<?php
-$data = array(
-	"email" => "user@example.com",
-	"password" => "12345678",
-	"url" => "https://api.wedding.izalul.com"
-);
-
-echo json_encode($data);
-?>
---
+++ js/config.js
@@ -0,0 +1,3 @@
+var email = "user@example.com";
+var password = "12345678";
+var url = "https://api.wedding.izalul.com";
